9f0aaa375a36cf98f635dd9a32c300aade466bb3
Net <Net@NetKBs>
2023-09-23 15:43:43 -0400

    base template for subjects and semesters routes

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,9 @@ import express from "express";
 import authRoutes from "./routes/auth.routes";
 import { errorHandler } from "./middlewares/errorHandler";
 require("express-async-errors");
-import usersRoutes from './routes/users.routes'
-// import semestersRoutes from './routes/semesters.routes'
-// import subjectsRoutes from './routes/subjects.routes'
+//import usersRoutes from './routes/users.routes'
+import semestersRoutes from './routes/semesters.routes'
+import subjectsRoutes from './routes/subjects.routes'
 // import assignmentsRoutes from './routes/assignments.routes'
 
 const app = express();
@@ -14,8 +14,8 @@ app.use(express.json());
 
 app.use("/api/auth", authRoutes);
 //app.use('/api/users', usersRoutes);
-// app.use('/api/semesters', semestersRoutes);
-// app.use('/api/subjects', subjectsRoutes);
+app.use('/api/semesters', semestersRoutes);
+app.use('/api/subjects', subjectsRoutes);
 // app.use('/api/assignments', assignmentsRoutes);
 
 app.listen(PORT, () => {
--- src/routes/subjects.routes.ts
+++ src/routes/subjects.routes.ts
@@ -1,8 +1,26 @@
 import { Router } from "express";
 import { prisma } from "../db";
+import bcrypt from "bcrypt";
+import { registerSchema } from "../types/Register";
+import parseBody from "../utils/parseBody";
+import { ErrorCode, HTTPError } from "../utils/HTTPError";
+import asyncHandler from "express-async-handler";
+import jwt from "jsonwebtoken";
 
 const router = Router();
 
 
+router.post("/login", asyncHandler(async (req, res) => {
+
+  
+
+  // res.status(201).json({
+  //   data: {
+  //     user: userData,
+  //   },
+  //   error: null,
+  // });
+
+}))
 
 export default router;
